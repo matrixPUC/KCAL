13f7a28edb466e88de2d690479efe12c537f1dcc
beatrizkhey <beatrizkhey@gmail.com>
2023-05-15 11:49:49 -0300

    Criação da pasta includes e alteração no perfil, home e css

---
+++ includes/header.inc.php
@@ -0,0 +1,36 @@
+<?php
+    include('../database/connection.php');
+    $nome_sessao = $_SESSION['nome'];
+    $email = $_SESSION['email'];
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/header.css">
+    <script src="../scripts/popup.js" defer></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+</head>
+<body>
+<header id="main-header">
+    <div class="content">
+        <div class="logo">
+            <img src="../assets/nutricionista.png" alt="">
+            <input type="text" placeholder="Pesquisar no KCal">
+        </div>
+        <nav class="menu-icons">
+            <div class="home"><span class="material-icons"><a href="./home.php">home</a></span></div>
+            <div><span class="material-icons">ondemand_video</span></div>
+            <div><span class="material-icons">storefront</span></div>
+            <div><span class="material-icons">supervised_user_circle</span></div>
+            <div><span class="material-icons">casino</span></div>
+        </nav>
+        <div class="user">
+            <div><span><a href="./dados_usuario.php"><?php echo $nome_sessao ?></a></span></div>
+            <nav class="actions"><div><span class="material-icons">textsms</span></div></nav>
+        </div>
+    </div>
+</header>
--- pages/home.php
+++ pages/home.php
@@ -4,148 +4,38 @@
     //ID do usuário
     $nome_sessao = $_SESSION['nome'];
 ?>
-
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Feed</title>
-  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-  <link rel="stylesheet" href="../style/home.css">
-  <link rel="stylesheet" href="../style/header.css">
-  
-
-</head>
-
-<body>
-  <header id="main-header">
-    <div class="content">
-      <div class="logo">
-        <img src="../assets/nutricionista.png" alt="">
-        <input type="text" placeholder="Pesquisar no KCal">
-      </div>
-
-      <nav class="menu-icons">
-
-        <div class="home">
-          <span class="material-icons">
-            <a href = "./home.php"> 
-              home
-            </a>    
-          </span>
-        </div>
-
-        <div>
-          <span class="material-icons">
-            ondemand_video
-          </span>
-        </div>
-
-        <div>
-          <span class="material-icons">
-            storefront
-          </span>
-        </div>
-
-        <div>
-          <span class="material-icons">
-            supervised_user_circle
-          </span>
-        </div>
-
-        <div>
-          <span class="material-icons">
-            casino
-          </span>
-        </div>
-
-
-      </nav>
-
-      <div class="user">
-        <div>
-          <?php 
-          echo '<span><a href="./perfil.php">' . $nome_sessao . '</a></span>'
-        
-          ?>
-        </div>
-
-        <nav class="actions">
-          <div><span class="material-icons">
-              textsms
-            </span></div>
-        </nav>
-
-      </div>
-    </div>
-  </header>
-
-  <div class="wrapper-content content">
-    <aside class="nav-icons">
-      <ul>
-
-        <li>
-          <img src="../assets/pessoa.png" alt="">
-          <?php 
-            echo '<strong><a href="./perfil.php">' . $nome_sessao . '</a></strong>'
-          ?>
-        </li>
-
-        <li>
-          <span class="material-icons">
-            assistant_photo
-          </span>
-          <strong><a href="./receita.php">Criar Receita</a></strong>
-        </li>
-
-        <li>
-          <span class="material-icons">
-            group_work
-          </span>
-          <strong><a href="./calcular.php">Calcular</a></strong>
-        </li>
-        <li>
-            <span class="material-icons">
-                logout 
-            </span>
-            <strong><a href="./logout.php">LogOut</a></strong>
-         </li>
-
-        <?php
-          if ($_SESSION['adm'] == true) {
-            echo '<li>
-                    <span class="material-icons">
-                      group_work
-                    </span>
-                    <strong><a href="./validar.php">Validar nutricionistas</a></strong>
-                 </li>';
-          }
-        ?>
-
-
-
-      </ul>
+    <?php include_once '../includes/header.inc.php';?>
+    <head>
+        <link rel="stylesheet" href="../style/home.css">
+        <title>Feed</title>
+    </head>
+    <div class="wrapper-content content">
+    <?php 
+        include_once '../includes/menu.inc.php';
+        if ($_SESSION['adm'] == true) {
+        echo '<li>
+                <span class="material-icons">
+                    group_work
+                </span>
+                <strong><a href="./validar.php">Validar nutricionistas</a></strong>
+                </li>';
+        }
+    ?>
+    </ul>
     </aside>
-
     <section class="feed">
-      
       <main>
         <ul>
           <?php
             //Conexão com o banco
             include('../database/connection.php');
-
             //ID do usuário
             $id_sessao = $_SESSION['ID'];
-
             //Match no SQL para obter as publicações em ordem de data descendente de perfis que o usuário segue
             $sql = "SELECT * FROM receita
                     WHERE public = true";
             $result = $mysqli->query($sql);
             $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
             //Loop de publicações
             if (sizeof($rows) > 0) {
               foreach ($rows as $row) {
@@ -169,9 +59,7 @@
                   </div>
                   <p>" . $row["nome"] . "</p>
                 </div>
-
                 <div class='image-feed'></div>
-
                 <div class='actions-user'>
                   <div>
                     <span class='material-icons'>
@@ -179,38 +67,28 @@
                     </span>
                     <strong>Curtir</strong>
                   </div>
-
                   <div>
                     <span class='material-icons'>
                       chat_bubble
                     </span>
                     <strong>Comentar</strong>
                   </div>
-
                   <div>
                     <span class='material-icons'>
                       share
                     </span>
                     <strong>Compartilhar</strong>
-
                   </div>
-
                 </div>
-
               </li>";
               }
             } else {
               echo "Sem publicações.";
             }
-    
           ?>
         </ul>
       </main>
     </section>
-
-    
   </div>
-
 </body>
-
 </html>
