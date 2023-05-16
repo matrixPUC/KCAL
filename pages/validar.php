3f8892db1d5d3b174503158407040bd749a067e3
lohine <lohine.mussi@gmail.com>
2023-05-15 22:26:32 -0300

    header

--- pages/validar.php
+++ pages/validar.php
@@ -4,8 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>Perfil</title>
+    
+
 </head>
+    <?php include_once '../includes/header.inc.php';?>
 <body>
     <h2>Nutricionistas para serem validados:</h2>
     
