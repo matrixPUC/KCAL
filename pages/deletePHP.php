73361ee626b2751af7858ed7d41cf363198677ad
joao vitor <joaovitoras146@gmail.com>
2023-06-12 19:57:06 -0300

    Arruma deleção de conta

--- pages/deletePHP.php
+++ pages/deletePHP.php
@@ -6,9 +6,24 @@
 
   try {
     $sql = "DELETE FROM nutricionista WHERE ID_usuario = '$ID'";
-  
+    $result = $mysqli->query($sql);
+
+    $sql = "DELETE FROM calculadora WHERE ID_usuario = '$ID'";
+    $result = $mysqli->query($sql);
+
+    $sql = "DELETE FROM publicacao WHERE ID_usuario = '$ID'";
+    $result = $mysqli->query($sql);
+
+    $sql = "DELETE FROM usuario_publicacao WHERE ID_usuario = '$ID'";
+    $result = $mysqli->query($sql);
+
+    $sql = "DELETE FROM receita WHERE ID_usuario = '$ID'";
+    $result = $mysqli->query($sql);
+
+    $sql = "DELETE FROM usuario_receita WHERE ID_usuario = '$ID'";
     $result = $mysqli->query($sql);
   } catch (Exception $e){};
+
 
   $sql = "DELETE FROM usuario WHERE ID = '$ID'";
 
